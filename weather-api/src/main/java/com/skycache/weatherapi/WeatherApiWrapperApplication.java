37ca474478b2fcfb599a83ab00f2b6ddebf1b937
mistyfun <softsunshine0912@gmail.com>
2024-09-04 22:18:02 +0200

    initial commit
    initial commit

---
+++ weather-api/src/main/java/com/skycache/weatherapi/WeatherApiWrapperApplication.java
@@ -0,0 +1,13 @@
+package com.skycache.weatherapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherApiWrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherApiWrapperApplication.class, args);
+	}
+
+}
---
+++ weather-api/src/test/java/com/skycache/weatherapi/WeatherApiWrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skycache.weatherapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApiWrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
