4f48ae5a694a5d87aad86173d77cc5204b6734f2
mistyfun <softsunshine0912@gmail.com>
2024-09-05 22:30:26 +0200

    add classes for future usage and clean up

--- weather-api/src/main/java/com/skycache/weatherapi/WeatherApiWrapperApplication.java
+++ weather-api/src/main/java/com/skycache/weatherapi/WeatherApiWrapperApplication.java
@@ -2,13 +2,15 @@ package com.skycache.weatherapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
 
 @SpringBootApplication
 public class WeatherApiWrapperApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherApiWrapperApplication.class, args);
-		System.out.print("Hello from springboot");
+		System.out.println("Hello from springboot");
 	}
 
 }
